cc0a90e5717b60f3432a8e9d18c1715131bb6624
Emmanuel Ndubuisi <emmanduu@live.com>
2026-09-21 20:15:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (/*#__PURE__*/
+    React.createElement("main", null, /*#__PURE__*/
+    React.createElement("header", null, /*#__PURE__*/
+    React.createElement("div", { class: "icon" }, "\u269B\uFE0F"), " Site Powered by React"
+    )
+    ));
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+root.render(/*#__PURE__*/React.createElement(App, null));
